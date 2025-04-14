ea2171b7c15fde6092a839a078991d08de1ddad4
Gaston Brondani <69480995+GastonBrondani@users.noreply.github.com>
2025-04-14 19:19:34 -0300

    Gaston (#14)
    * actualizacion de codigo viejo
    
    * control de errores de cliente
    
    ---------
    
    Co-authored-by: GastonBrondani <gaston.brondani@gmail.com>

--- BackendMada/Models/cliente.cs
+++ BackendMada/Models/cliente.cs
@@ -12,19 +12,25 @@ public partial class cliente
 {
     [Key]
     public int id_cliente { get; set; }
-
-    [StringLength(40)]
+    
+    [Required(ErrorMessage = "Debe poner un nombre al cliente")]
+    [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
     public string nombre { get; set; } = null!;
 
-    [StringLength(50)]
+    [Required(ErrorMessage = "El cliente debe tener una direccion")]
+    [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
     public string direccion { get; set; } = null!;
+    
+    //tendriamos que buscar una forma de calcular el cuit, que exista
+    [Required(ErrorMessage = "El cliente tiene que tener un cuit/cuil")]
+    [StringLength(11, ErrorMessage = "verifique el cuit")]
+    public string cuit_cuil { get; set; }
 
-    public int cuit_cuil { get; set; }
-
-    [StringLength(50)]
+    [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
     public string email { get; set; } = null!;
 
-    [StringLength(30)]
+    [Required(ErrorMessage = "Debe ingresar un numero de telefono al cliente")]
+    [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
     public string telefono { get; set; } = null!;
 
     [InverseProperty("id_clienteNavigation")]
